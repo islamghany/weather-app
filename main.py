00ecfec18e1d71295bb89403dce1fa6bbdc9ace9
Islam Mostafa <islamghany3@gmail.com>
2021-03-15 19:41:59 +0200

    init packages

---
+++ main.py
@@ -0,0 +1,12 @@
+import fastapi
+import uvicorn
+
+app = fastapi.FastAPI()
+
+@app.get('/')
+def index():
+    return 'Hello world'
+
+
+if __name__ == '__main__':
+    uvicorn.run(app,host="localhost",port=5000)
