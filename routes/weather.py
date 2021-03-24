d820caad2024ec7e25008cd79a9ae4a02b6f5a90
Islam Mostafa <islamghany3@gmail.com>
2021-03-24 15:32:48 +0200

    add reports

--- routes/weather.py
+++ routes/weather.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter,Depends,Response
 from models.location import Location
 from api.openweather import get_report
+from api import report
+from typing import Optional, List
+
 from utils import cache
-from models.error import Error 
+from models.error import Error
+from models.reports import Report, ReportSubmittal
 
 router = APIRouter()
 
@@ -18,3 +22,15 @@ async def weather(loc:Location= Depends()):
     cache.set_weather(loc.city,loc.state,loc.country,loc.units,report)
     print('fetchig')
     return report
+
+@router.get('/api/reports', name='all_reports', response_model=List[Report])
+async def reports_get() -> List[Report]:
+    return await report.get_reports()
+
+
+@router.post('/api/reports', name='add_report', status_code=201, response_model=Report)
+async def reports_post(report_submittal: ReportSubmittal) -> Report:
+    d = report_submittal.description
+    loc = report_submittal.location
+
+    return await report.add_report(d, loc)
